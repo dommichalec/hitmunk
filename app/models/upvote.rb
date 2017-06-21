9b8617742be5425dcf0670cbce4436b3298fbec0
Dominic Michalec <dominicjjmichalec@gmail.com>
2017-06-20 21:30:33 -0400

    add validations to upvote model

--- app/models/upvote.rb
+++ app/models/upvote.rb
@@ -1,4 +1,8 @@
 class Upvote < ApplicationRecord
+  # Active Record Associations
   belongs_to :link
   belongs_to :user
+
+  # Active Record Validations
+  validates :user_id, :link_id, presence: true
 end
--- test/models/upvote_test.rb
+++ test/models/upvote_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UpvoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @link = links(:honnold)
+  end
 end
