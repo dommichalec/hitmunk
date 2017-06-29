ef6682508a0ba2d644c38b4053c8f4aaebf4ca61
Dominic Michalec <dominicjjmichalec@gmail.com>
2017-06-29 16:51:27 -0400

    remove worker

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: bundle exec puma -C config/puma.rb
-commentworker: bundle exec sidekiq -c 1
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,4 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   Rails.application.routes.default_url_options[:host] = 'https://hitmunkbeta.herokuapp.com'
-  config.action_mailer.smtp_settings = { address: 'https://hitmunkbeta.herokuapp.com' }
-
 end
